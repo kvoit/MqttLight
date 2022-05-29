6476c94292a112c70e98f4c7baf6b670cf319c1d
kvoit <kayx+github.com@voits.net>
2022-05-29 12:24:33 +0200

    Added ESP32 support to hpp

--- src/MqttMOSFETLight.hpp
+++ src/MqttMOSFETLight.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include<MqttBaseLight.hpp>
+#include<Arduino.h>
 
 class MqttMOSFETLight : public MqttBaseLight
 {
 public:
-    MqttMOSFETLight(uint8_t pin_a, MqttController &mqtt, const char *mqtt_topic_control_state_a, uint8_t defaultLevel_a, uint16_t min_pwm_a, uint16_t max_pwm_a);
+    MqttMOSFETLight(uint8_t pin_a, MqttController &mqtt, const char *mqtt_topic_control_state_a, uint8_t defaultLevel_a, uint16_t min_pwm_a, uint16_t max_pwm_a, uint8_t channel=0);
     
-    const uint8_t pin;
+    uint8_t pin;
     
     void commit();
     void begin(const uint8_t brightness);
