52ea77f061db6a5b4540a6ff91df0d1d4a890b6f
Kay-Michael Voit <kay-michael.voit@caesar.de>
2023-01-26 13:49:08 +0100

    Moved ESP32 LEDC to begin

--- src/MqttMOSFETLight.cpp
+++ src/MqttMOSFETLight.cpp
@@ -2,19 +2,8 @@
 
 /* MqttMOSFETLight */
 MqttMOSFETLight::MqttMOSFETLight(uint8_t pin_a, MqttController &mqtt, const char *mqtt_topic_base_a, uint8_t defaultLevel_a, uint16_t min_pwm_a, uint16_t max_pwm_a, int8_t channel, int32_t freq, int8_t res)
-        : MqttBaseLight(mqtt, mqtt_topic_base_a, defaultLevel_a, min_pwm_a, max_pwm_a), pin(pin_a)
+        : MqttBaseLight(mqtt, mqtt_topic_base_a, defaultLevel_a, min_pwm_a, max_pwm_a), pin(pin_a), channel(channel), freq(freq), res(res)
 { 
-    #ifdef ESP32
-    if(channel>-1) {
-        ledcAttachPin(pin_a, channel);
-        pin = channel;
-    }
-    if(freq>-1 && res>-1) {
-        ledcSetup(pin, freq, res);
-    }
-    #else
-    ;
-    #endif
 }
 
 void MqttMOSFETLight::commit()
@@ -37,7 +26,22 @@ void MqttMOSFETLight::commit()
 
 void MqttMOSFETLight::begin(const uint8_t brightness)
 {
+    #ifdef ESP32
+    if(channel>-1) {
+        Serial.println("ledcAttachPin");
+        ledcAttachPin(pin, channel);
+        pin = channel;
+    }
+    if(freq>-1 && res>-1) {
+        Serial.println("ledcSetup");
+        ledcSetup(pin, freq, res);
+    }
+    #else
+    ;
+    #endif
+
     digitalWrite(pin, LOW);
     pinMode(pin, OUTPUT);
+    
     MqttBaseLight::begin(brightness);
 }
--- src/MqttMOSFETLight.hpp
+++ src/MqttMOSFETLight.hpp
@@ -9,6 +9,9 @@ public:
     MqttMOSFETLight(uint8_t pin_a, MqttController &mqtt, const char *mqtt_topic_control_state_a, uint8_t defaultLevel_a, uint16_t min_pwm_a, uint16_t max_pwm_a, int8_t channel=-1, int32_t freq=-1, int8_t res=-1);
     
     uint8_t pin;
+    int8_t channel=-1;
+    int32_t freq=-1;
+    int8_t res=-1;
     
     void commit();
     void begin(const uint8_t brightness);
